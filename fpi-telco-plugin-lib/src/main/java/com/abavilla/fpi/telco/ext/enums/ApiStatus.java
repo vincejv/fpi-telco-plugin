b9aadb9742a4111e9e1b8334c3ecb102c266cc79
Vince Jerald Villamora <vincevillamora@gmail.com>
2022-11-12 04:36:32 +0800

    fix(enums): move package to root

--- fpi-telco-plugin-lib/src/main/java/com/abavilla/fpi/telco/ext/enums/ApiStatus.java
+++ fpi-telco-plugin-lib/src/main/java/com/abavilla/fpi/telco/ext/enums/ApiStatus.java
@@ -16,7 +16,7 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.     *
  ******************************************************************************/
 
-package com.abavilla.fpi.telco.ext.entity.enums;
+package com.abavilla.fpi.telco.ext.enums;
 
 import java.util.EnumSet;
 import java.util.HashMap;
